acabf743cf34fbd1378d9e860964099aa818111d
Shodvarz Shutiev <sshutiev@gmail.com>
2022-11-17 20:09:10 +0300

    tuple

--- git2.py
+++ git2.py
@@ -1,2 +1,8 @@
-for i in range(4):
-    print(i)
+import random
+i = 0
+b = []
+while i < 10:
+    b.append(random.randint(1,10))
+    i += 1
+b = tuple(b)
+print('max = ', max(b),'и'  'min = ', min(b))
