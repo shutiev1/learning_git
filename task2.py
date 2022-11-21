9a40b85fde50e9bb59d343c2b64cc6cd274085f8
Shodvarz Shutiev <sshutiev@gmail.com>
2022-11-21 14:40:51 +0300

    changed_solution

--- task2.py
+++ task2.py
@@ -2,10 +2,5 @@ import random
 tuple1 = tuple(random.randint(0, 5) for i in range(10))
 tuple2 = tuple(random.randint(-5, 0) for i in range(10))
 tuple3 = tuple1 + tuple2
-
-count = 0
-for i in tuple3:
-    if i == 0:
-        count += 1
-print("кортеж:", tuple3)
-print("количество нулей:", count)
+print(tuple3)
+print(tuple3.count(0))
